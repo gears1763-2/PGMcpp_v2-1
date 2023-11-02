8edc45547daa07cc9666c7d494c7d328876a0a67
gears1763-2 <91225514+gears1763-2@users.noreply.github.com>
2023-11-02 12:43:24 -0700

    Still working on Python 3 bindings portability ...

--- pybindings/setup.py
+++ pybindings/setup.py
@@ -5,17 +5,25 @@
     
     ref: https://pybind11.readthedocs.io/en/stable/compiling.html
     ref: https://github.com/pybind/python_example/blob/master/setup.py
+    ref: https://github.com/pybind/pybind11/blob/master/pybind11/setup_helpers.py
     ref: https://stackoverflow.com/questions/64261546/how-to-solve-error-microsoft-visual-c-14-0-or-greater-is-required-when-inst
+    ref: https://docs.python.org/3.5/library/sys.html#sys.platform
 """
 
 
 import os
+import shutil
+import sys
+
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension
 
 
 # remove any existing extensions
 for root, dirs, files in os.walk(os.getcwd()):
+    for name in dirs:
+        if "build" in name:
+            shutil.rmtree(name)
     for name in files:
         if "PGMcpp" in name:
             os.remove(name)
@@ -34,7 +42,7 @@ for root, dirs, files in os.walk(".." + os.sep + "source"):
 ext_modules = [
     Pybind11Extension(
         "PGMcpp",
-        sources=source_list
+        sources=source_list,
     )
 ]
 
@@ -47,3 +55,10 @@ setup(
     python_requires=">=3.7",
     install_requires=["setuptools>=42", "pybind11>=2.6.1"]
 )
+
+
+# clean up
+for root, dirs, files in os.walk(os.getcwd()):
+    for name in dirs:
+        if "build" in name:
+            shutil.rmtree(name)
